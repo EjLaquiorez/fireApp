b82af019e555138adcbe8cc6df6f111f12170e4b
EjLaquiorez <ejlqrz@gmail.com>
2024-11-23 14:28:27 +0800

    last update before branching

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('chart/', PieCountbySeverity, name='chart'),
-
+    path('chart/pie/', PieCountbySeverity, name='pie-chart'),  # Ensure proper path here
+    path('chart/line/', LineCountbyMonth, name='line-chart'),
+    path('chart/multiline/', MultilineIncidentTop3Country, name='multiline-chart'),
+    path('chart/bar/', multipleBarbySeverity, name='bar-chart'),
+    path('chart.html', views.chart_view, name='chart-html'),  # This should be defined
 ]
+
+    
